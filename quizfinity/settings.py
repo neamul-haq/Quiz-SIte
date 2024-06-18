0b614326b515402a324b4406120d32527c2d156e
Neamul <neamul.cse6.bu@gmail.com>
2024-06-18 22:22:37 +0600

    debug false ; ;

--- quizfinity/settings.py
+++ quizfinity/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%#)9rrxaac(z&2*ert*3$2hl63%&ijwgb3-1z5%=sgiq3ek%fs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
